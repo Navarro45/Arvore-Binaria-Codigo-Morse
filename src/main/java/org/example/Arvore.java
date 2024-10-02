86f03ddcb2dfedb7f322b9b9cfce0d397fc302df
Navarro45 <127626860+Navarro45@users.noreply.github.com>
2024-10-02 10:59:12 -0300

    Added Menu

--- src/main/java/org/example/Arvore.java
+++ src/main/java/org/example/Arvore.java
@@ -53,5 +53,53 @@ public class Arvore {
         return resultado.toString();
     }
 
+    public void menu() {
+        Boolean loop = true;
+        Scanner entrada = new Scanner(System.in);
+
+        while (loop) {
+            try {
+                System.out.println("Bem-vindo à árvore do morse");
+                System.out.println("Para inserir uma letra, digite 1 ");
+                // System.out.println("Para remover uma letra digite 2");
+                System.out.println("Para buscar uma letra, digite 3");
+                System.out.println("Para encerrar o programa, digite 9");
+                int escolha = entrada.nextInt();
+                entrada.nextLine();  // Limpar o buffer após a leitura de números
+
+                switch (escolha) {
+                    case 1:
+                        System.out.println("Utilize espaço entre os símbolos do código.");
+                        System.out.println("Digite a letra desejada em código morse:");
+                        String codigo = entrada.nextLine();
+                        System.out.println("Insira a letra que o código inserido representa:");
+                        String caracter = entrada.nextLine();
+                        this.inserir(codigo, caracter);
+                        break;
+
+                    case 3:
+                        System.out.println("Utilize espaço entre os símbolos do código.");
+                        System.out.println("Digite a letra desejada em código morse:");
+                        String morse = entrada.nextLine();
+                        this.buscar(morse);
+                        break;
+
+                    case 9:
+                        System.out.println("Encerrando...");
+                        loop = false;
+                        break;
+
+                    default:
+                        // Caso a escolha seja fora das opções válidas
+                        System.out.println("Opção inválida. Por favor, escolha uma opção válida.");
+                        break;
+                }
+            } catch (Exception e) {
+                // Tratamento de outros erros inesperados
+                System.out.println("Ocorreu um erro: " + e.getMessage());
+            }
+        }
+        entrada.close(); // Fechar o scanner ao sair do loop
+    }
 
 }
