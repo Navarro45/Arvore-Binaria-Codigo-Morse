28d11440a21c8e0c64b908653f36cc1c987dc4cd
PedroNavarro <phvnavarro@gmail.com>
2024-10-02 10:39:31 -0300

    Menu e tratamento de erro

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,11 +4,15 @@ public class Main {
     public static void main(String[] args) {
         Arvore tree = new Arvore();
         tree.iniciaizar();
-        tree.inserir(". . .", "s");
-        tree.inserir("- - -", "o");
+        tree.menu();
 
-        System.out.println(tree.buscar(" . . ."));
-        System.out.println(tree.buscar(" - - -"));
-        System.out.println(tree.buscar(". . . - - - . . ."));
+
+        //tree.inserir(". . .", "s");
+        //tree.inserir("- - -", "o");
+
+        //System.out.println(tree.buscar(" . . ."));
+        //System.out.println(tree.buscar(" - - -"));
+        //System.out.println(tree.buscar(" . . ."));
+        //System.out.println(tree.buscar(". . . - - - . . ."));
     }
 }
