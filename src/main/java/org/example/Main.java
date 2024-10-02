845d49e2f1765cc4dd1c0220a1dceed8edc7eac6
Navarro45 <127626860+Navarro45@users.noreply.github.com>
2024-10-02 10:59:49 -0300

    Added Menu method

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,11 +4,6 @@ public class Main {
     public static void main(String[] args) {
         Arvore tree = new Arvore();
         tree.iniciaizar();
-        tree.inserir(". . .", "s");
-        tree.inserir("- - -", "o");
-
-        System.out.println(tree.buscar(" . . ."));
-        System.out.println(tree.buscar(" - - -"));
-        System.out.println(tree.buscar(". . ./- - -/. . ."));
+        tree.menu();
     }
 }
